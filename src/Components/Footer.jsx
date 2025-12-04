4da60c29b2562d6b5b4a12b17ead05253f453687
Cody.McFarland <codymcfarland@protonmail.com>
2025-12-04 13:05:41 -0500

    save

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2025 Car Dealership Project. Justin, Felix, Khadija, and Cody. All rights reserved.</p>
+        </footer>
+    );
+}
+export default Footer;
