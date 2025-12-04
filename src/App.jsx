db283deaf5334e348741102a89a5b87ea8ff488c
Sadat-Khadija <khadija.sadat9815@gmail.com>
2025-12-04 10:10:38 -0800

    Revert "add page"
    This reverts commit 944839eaecca38253f982ae606f6c5981359a234.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 // import NavBar from './Components/NavBar.jsx';
 import Router from './Services/Router.jsx';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        {/* <NavBar /> */}
-        <Router />
-      </div>
-    </BrowserRouter>
-  );
+   return (
+    <>
+      <BrowserRouter>
+        <div className='App'>
+          {/* <NavBar /> */}
+          <Router />
+          {/* <h1>Hello World!</h1> */}
+        </div>
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
