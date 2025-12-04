c929dc20c6096a6de0301a4d020e02f3b0642ee8
Cody.McFarland <codymcfarland@protonmail.com>
2025-12-04 16:47:51 -0500

    Added GA logo to footer :)

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,7 @@
 const Footer = () => {
     return (
         <footer>
+            <img src="https://imgs.search.brave.com/J8dIZgvMxQakBvrLf1CEBlKEjtUA7fFVn_HYFfuEMLw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pY29u/YXBlLmNvbS93cC1j/b250ZW50L3BuZ19s/b2dvX3ZlY3Rvci9n/ZW5lcmFsLWFzc2Vt/Ymx5LWxvZ28ucG5n" alt="" width="100" />
             <p>&copy; 2025 Car Dealership Project. Justin, Felix, Khadija, and Cody. All rights reserved.</p>
         </footer>
     );
